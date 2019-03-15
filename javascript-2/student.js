2bd26c206b90bba0ae14fd3904cd7efa1428e064
Lee Odell <leerodell@gmail.com>
2019-03-15 12:19:06 -0700

    web week2 skillscheck js2 complete

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -4,26 +4,37 @@
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
 
+var lovesCode = true;
 // Code Here
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
+ var age = 99;
 
 // Code Here
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
 
+var name = "Leo"
 // Code Here
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
 
+var phrase = "";
 // Code Here
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
 
+function js2(){
+if (lovesCode = true){
+    return phrase = "I love code"
+}
+else {return phrase = "I do not love to code"}}
+
+js2(phrase)
 // Code Here
 
 //////////////////Wrap Up////////////////////
